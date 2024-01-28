afc4a431b0762cd651a591681a4045bbb070ab7a
Rishab Kumar <rishabkumar7@gmail.com>
2024-01-27 21:55:37 -0500

    cleaned up pets example

--- pets-name/main.py
+++ pets-name/main.py
@@ -1,5 +1,4 @@
 #Imports
-import streamlit as st
 from langchain_openai import OpenAI
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
@@ -18,12 +17,10 @@ def generate_pet_name(animal_type, pet_color, openai_api_key):
   Answer: Here are five names for your pet:
   """
 
-  prompt_template= PromptTemplate(template=template, input_variables=["animal_type", "pet_color"])
-  #prompt_template = PromptTemplate.from_template("I have {animal_type} pet and I want a cool name for it, it is {pet_color} color. Suggest me five cool names for my pet"
-  #)
+  prompt_template = PromptTemplate.from_template("I have {animal_type} pet and I want a cool name for it, it is {pet_color} color. Suggest me five cool names for my pet"
+  )
 
-  #name_chain = prompt_template | llm
-  name_chain = LLMChain(llm=llm, prompt=prompt_template)
+  name_chain = prompt_template | llm
 
   response = name_chain.invoke({"animal_type":animal_type, "pet_color":pet_color})
 
