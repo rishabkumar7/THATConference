ffd3e54fbb0291b1844c3e1254a416390ba0f85c
rishabkumar7 <rishabkumar7@gmail.com>
2024-02-11 09:52:03 -0500

    added more query examples

--- youtube-assistant/app.py
+++ youtube-assistant/app.py
@@ -1,12 +1,12 @@
 import main as youtube
 import textwrap
 
-
+# Add your YouTube video URL
 youtube_url="https://www.youtube.com/watch?v=1p6P471PBx0"
-#query="What did Satya say about AI?"
 
-query = "How old is Satya in 2024?"
-
+# Different Queries
+query="What did Satya say about AI?"
+#query = "How old is Satya in 2024?"
 #query="Did Satya mention Google?"
 #query="Did Satya mention anything about TikTok?"
 
